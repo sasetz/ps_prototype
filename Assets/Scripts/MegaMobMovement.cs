6bf46fa86cf97de6c7f37b0d3b718dcab5f23a43
Sasetz <56696451a@gmail.com>
2025-01-28 21:48:19 +0100

    Added basic AI movement

--- Assets/Scripts/MegaMobMovement.cs
+++ Assets/Scripts/MegaMobMovement.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Sosal : MonoBehaviour
+public class MegaMobMovement : MonoBehaviour
 {
+    public Transform goal;
     // Start is called before the first frame update
     void Start()
     {
-        
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
     }
 
     // Update is called once per frame
